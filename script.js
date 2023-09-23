f3ac2413fb7b7b70b1950715187da775fda0a32d
adithyan-ks <144652102+adithyan-ks@users.noreply.github.com>
2023-09-23 23:42:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function() {
+    var image1_bg_color = '#317a5c';
+    var image2_bg_color = '#dedede';
+    var image3_bg_color = '#ff0000';
+    var image4_bg_color = '#000000';
+  
+  // pseudo-code...
+  
+  // When #image2 becomes visible fade .container bg color from image1_bg_color to image2_bg_color
+  
+  // When #image3 becomes visible fade .container bg color from image2_bg_color to image3_bg_color
+  
+  // When #image4 becomes visible fade .container bg color from image3_bg_color to image4_bg_color
+  
+  // When #image1 re-enters view fade .container bg color from image4_bg_color to image1_bg_color
+  
+  // etc.
+  });
